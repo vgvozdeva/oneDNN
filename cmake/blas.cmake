521f758a5a75fd8ad46590ccce914fab3caa35b0
viktoria.gvozdeva <vgvozdev@jfldnn043.jf.intel.com>
2025-10-20 04:52:32 -0700

    tests: changes year for include and examples folder

--- cmake/blas.cmake
+++ cmake/blas.cmake
@@ -1,7 +1,6 @@
 # *******************************************************************************
 # Copyright 2020-2021 Arm Limited and affiliates.
 # Copyright 2024 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
