b3181fc23bca383cfbabd863e837f2ec834811e8
Lv, Tao A <tao.a.lv@intel.com>
2026-03-16 23:21:20 -0700

    gtests: internals: include sdpa iface header

--- tests/gtests/internals/sdpa_internal.hpp
+++ tests/gtests/internals/sdpa_internal.hpp
@@ -19,46 +19,9 @@
 
 #include "dnnl.hpp"
 
+#include "common/sdpa_test_iface.hpp"
+
 // NOLINTBEGIN(readability-identifier-naming)
-
-/// Creates a primitive descriptor for a scaled dot product attention primitive
-///
-/// @param primitive_desc Output primitive descriptor.
-/// @param engine Engine to use.
-/// @param query_desc Query memory descriptor (tensor Q)
-/// @param key_desc Key memory descriptor (tensor K)
-/// @param value_desc Value memory descriptor (tensor V)
-/// @param dst_desc Destination memory descriptor.
-/// @param attn_mask_desc Attention mask memory descriptor.
-/// @param attr Primitive attributes (can be NULL).
-/// @param kq_attr Attribute for the Key/Query matmul operation(can be NULL).
-/// @param vs_attr Attribute for the Value/Score matmul operation(can be NULL).
-/// @returns #dnnl_success on success and a status describing the error
-///     otherwise.
-
-dnnl_status_t DNNL_API sdpa_primitive_desc_create(
-        dnnl_primitive_desc_t *primitive_desc_iface, dnnl_engine_t engine,
-        const_dnnl_memory_desc_t query_desc, const_dnnl_memory_desc_t key_desc,
-        const_dnnl_memory_desc_t value_desc, const_dnnl_memory_desc_t dst_desc,
-        const_dnnl_memory_desc_t mask_desc, const_dnnl_memory_desc_t scale_desc,
-        bool invert_scale, dnnl_dim_t kv_head_number, int attn_mask_type,
-        dnnl_alg_kind_t softmax_alg, dnnl_prop_kind_t prop,
-        const_dnnl_primitive_attr_t attr, const_dnnl_primitive_attr_t kq_attr,
-        const_dnnl_primitive_attr_t vs_attr);
-
-dnnl_status_t DNNL_API sdpa_primitive_desc_create(
-        dnnl_primitive_desc_t *primitive_desc_iface, dnnl_engine_t engine,
-        const_dnnl_memory_desc_t query_desc, const_dnnl_memory_desc_t key_desc,
-        const_dnnl_memory_desc_t value_desc, const_dnnl_memory_desc_t dst_desc,
-        const_dnnl_memory_desc_t diff_query_desc,
-        const_dnnl_memory_desc_t diff_key_desc,
-        const_dnnl_memory_desc_t diff_value_desc,
-        const_dnnl_memory_desc_t diff_dst_desc,
-        const_dnnl_memory_desc_t dS_desc, const_dnnl_memory_desc_t mask_desc,
-        const_dnnl_memory_desc_t scale_desc, bool invert_scale,
-        dnnl_dim_t kv_head_number, int attn_mask_type,
-        dnnl_alg_kind_t softmax_alg, const_dnnl_primitive_attr_t attr,
-        const dnnl_primitive_desc *hint_fwd_pd);
 
 namespace dnnl {
 namespace impl {
