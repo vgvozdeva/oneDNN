7f0a3e02c55c1ad505c51db1897286b2053a21dd
Lv, Tao A <tao.a.lv@intel.com>
2026-04-22 20:59:04 -0700

    graph: backend: dnnl: kernels: fix vector assign
    workaround a compiler warning -Werror=stringop-overread in GCC12

--- src/graph/backend/dnnl/kernels/sdp_bwd_primitive.cpp
+++ src/graph/backend/dnnl/kernels/sdp_bwd_primitive.cpp
@@ -236,7 +236,7 @@ status_t sdp_bwd_primitive_kernel_t::ocl_execute_impl(const stream_t *g_stream,
         if (subgraph_->is_constant_[i]) continue;
         returned_event = subgraph_->execs_[i]->execute_ocl(
                 p_stream, res->get_exec_args()[i], deps);
-        deps = {returned_event};
+        deps.assign(1, returned_event);
     }
 
     scratchpad.set_deps(returned_event);
